1ab85613e8436b63724b7b03680aabf5fe9521d0
Martin Pettersson <=>
2021-08-22 18:17:26 +0300

    changing raycolor into sphere

--- hello.cpp
+++ hello.cpp
@@ -4,12 +4,6 @@
 
 #include <iostream>
 
-color ray_color(const ray& r) {
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.8*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.2, 1.0, 0.4);
-}
-
 bool hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
@@ -17,6 +11,14 @@ bool hit_sphere(const point3& center, double radius, const ray& r) {
     auto c = dot(oc, oc) - radius*radius;
     auto discrimant = b*b - 4*a*c;
     return (discrimant > 0);
+}
+
+color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1-0 - t) * color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
 int main() {
