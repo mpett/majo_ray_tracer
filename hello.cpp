a96f2da6aeb9d6394ae7a98b655476c176cf8e72
Martin Pettersson <=>
2021-08-28 07:57:05 +0300

    addition multisample for antialiasing

--- hello.cpp
+++ hello.cpp
@@ -3,22 +3,9 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
  
 #include <iostream>
-
-/* double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = half_b * half_b - a*c;
-    
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-half_b - sqrt(discriminant) ) / a;
-    }
-} */
 
 color ray_color(const ray& r, hittable& world) {
     hit_record rec;
@@ -38,6 +25,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -45,6 +33,7 @@ int main() {
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     // Camera
+    camera cam;
 
     auto viewport_height = 2.0;
     auto viewport_width = aspect_ratio * viewport_height;
@@ -62,13 +51,18 @@ int main() {
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-	    auto u = double(i) / (image_width-1);
-	    auto v = double(j) / (image_height-1);
-	    ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width-1);
+                auto v = (j + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel); 
         }
     }
+
+    // Kolla om multisampel kan optimeras framöver genom att pre-rendera utan multisampel och sedan sampla endast pixlar för hittable object
 
     std::cerr << "\nDone.\n";
 }
