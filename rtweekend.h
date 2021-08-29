074599fd5abc3f2941a5e05c9070cd1f80d7954b
Martin Pettersson <=>
2021-08-29 20:56:51 +0300

    Final image, book1

--- rtweekend.h
+++ rtweekend.h
@@ -25,8 +25,15 @@ inline double degrees_to_radians(double degrees) {
 }
 
 inline double random_double() {
-    return rand() / (RAND_MAX + 1.0);
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
 }
+
+
+//inline double random_double() {
+//    return rand() / (RAND_MAX + 1.0);
+//}
 
 inline double random_double(double min, double max) {
     return min + (max - min) * random_double();
@@ -38,10 +45,7 @@ inline double clamp (double x, double min, double max) {
     return x;
 }
 
-// inline double random_double() {
-//    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-// } 
-
+ 
 // Common Headers
 
 #include "ray.h"
