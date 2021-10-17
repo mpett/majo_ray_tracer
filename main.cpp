a265c119f838bd5a5b898ea5d3783382c8debf2b
professor-oats <chronophob@gmail.com>
2021-10-17 17:36:28 +0300

    mixture_pdf

--- main.cpp
+++ main.cpp
@@ -32,11 +32,17 @@ color ray_color(const ray& r, const color& background, const hittable& world, sh
     if (!rec.mat_ptr->scatter(r, rec, albedo, scattered, pdf_val)) {
         return emitted;
     }
+    auto p0 = make_shared<hittable_pdf>(lights, rec.p);
+    auto p1 = make_shared<cosine_pdf>(rec.normal);
+    mixture_pdf mixed_pdf(p0, p1);
 
-    hittable_pdf light_pdf(lights, rec.p);
+    scattered = ray(rec.p, mixed_pdf.generate(), r.time());
+    pdf_val = mixed_pdf.value(scattered.direction());
+
+/*    hittable_pdf light_pdf(lights, rec.p);
     scattered = ray(rec.p, light_pdf.generate(), r.time());
     pdf_val = light_pdf.value(scattered.direction());
-
+*/
 
 /*    cosine_pdf p(rec.normal);
     scattered = ray(rec.p, p.generate(), r.time());
